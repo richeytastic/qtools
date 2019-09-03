b9bc887b45f183775b3091d11fc54792e7deb329
Richard Palmer <richeytastic@gmail.com>
2019-09-03 23:13:55 +0800

    Changed build script variable type.

--- cmake/QToolsConfig.cmake
+++ cmake/QToolsConfig.cmake
@@ -28,7 +28,7 @@ include( "${CMAKE_CURRENT_LIST_DIR}/Macros.cmake")
 get_library_suffix( _lsuff)
 set( _hints QTools${_lsuff} libQTools${_lsuff})
 find_library( QTools_LIBRARIES NAMES ${_hints} PATHS "${QTools_LIBRARY_DIR}/static" "${QTools_LIBRARY_DIR}")
-set( QTools_LIBRARIES     ${QTools_LIBRARIES}         CACHE FILE "The QTools imported libraries to link to.")
+set( QTools_LIBRARIES     ${QTools_LIBRARIES}         CACHE FILEPATH "The QTools imported libraries to link to.")
 
 # handle QUIETLY and REQUIRED args and set QTools_FOUND to TRUE if all listed variables are TRUE
 include( "${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake")
